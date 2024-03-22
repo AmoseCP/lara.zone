f4f48e349ecbdbeb0b4376fdcf5eb6ce70e85ec3
AmoseCP <amose.d@christianpost.com>
2024-03-22 11:41:00 -0400

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div className="flex justify-center">
       <div >
-        <Image src="/img/1.jpg" alt="picture" width={1200} height={500}/>
+        <h1>正在开发当中。。。请耐心等待！！！</h1>
+        <Image src="/img/1.jpg" alt="picture" width={500} height={500}/>
       </div>
     </div>
   );
